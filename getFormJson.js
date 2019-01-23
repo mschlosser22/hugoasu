b8c1eb259f95b723010a4f9438d5e551fc93d8d1
Mike Schlosser <ms122r@gmail.com>
2019-01-23 14:27:13 -0500

    test

---
+++ getFormJson.js
@@ -0,0 +1,9 @@
+const request = require('request');
+const fs = require('fs');
+
+request('http://dev.polsone.cds-store.com/api/v1/forms', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body);
+  let data = JSON.stringify(body['step-forms']);
+  fs.writeFileSync('data/forms/polsone/forms.json', data);
+});
