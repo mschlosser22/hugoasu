d612d6edc050064b6c85f9e2908de20f1f80b678
Mike Schlosser <ms122r@gmail.com>
2019-01-24 11:52:24 -0500

    switch to lead forms

--- getFormJson.js
+++ getFormJson.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 request('http://dev.polsone.cds-store.com/api/v1/forms', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   console.log(body);
-  let data = JSON.stringify(body['step-forms']);
+  let data = JSON.stringify(body['leads']);
   fs.writeFileSync('data/forms/polsone/forms.json', data);
 });
