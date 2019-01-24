ac469569e69ff36ff8d31b0e7af583079cfa754c
Mike Schlosser <ms122r@gmail.com>
2019-01-24 14:24:42 -0500

    gather asu programs

--- getFormJson.js
+++ getFormJson.js
@@ -1,9 +1,18 @@
 const request = require('request');
 const fs = require('fs');
 
+// Polsone Forms
 request('http://dev.polsone.cds-store.com/api/v1/forms', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   console.log(body);
   let data = JSON.stringify(body);
   fs.writeFileSync('data/forms/polsone/forms.json', data);
+});
+
+// ASU Programs
+request('https://asuonline.asu.edu//lead-submissions-v3.3/programs', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body);
+  let data = JSON.stringify(body);
+  fs.writeFileSync('data/programs/asu-programs.json', data);
 });
