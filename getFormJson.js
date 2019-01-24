c4ecbbbb3de9a07c39a86045de94943636fdd6ef
Mike Schlosser <ms122r@gmail.com>
2019-01-24 17:39:40 -0500

    add interest area data

--- getFormJson.js
+++ getFormJson.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 // Polsone Forms
 request('http://dev.polsone.cds-store.com/api/v1/forms', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
-  console.log(body);
+  //console.log(body);
   let data = JSON.stringify(body);
   fs.writeFileSync('data/forms/polsone/forms.json', data);
 });
@@ -12,7 +12,23 @@ request('http://dev.polsone.cds-store.com/api/v1/forms', { json: true }, (err, r
 // ASU Programs
 request('https://asuonline.asu.edu//lead-submissions-v3.3/programs', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
-  console.log(body);
+  //console.log(body);
   let data = JSON.stringify(body);
   fs.writeFileSync('data/programs/asu-programs.json', data);
+});
+
+// ASU Interest Areas
+request('https://asuonline.asu.edu//lead-submissions-v3.3/programs', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  let data = [];
+  body.forEach(function(element){
+  	let interestareas = element.interestareas;
+  	interestareas.forEach(function(area) {
+  		data.push(area);
+  	});
+  });
+  let unique = [...new Set(data)];
+  unique = JSON.stringify(unique);
+  console.log(unique);
+  fs.writeFileSync('data/programs/interestareas.json', unique);
 });
