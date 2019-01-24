1eb7d7f541a3cef4a70c6945e78c7c206fa24071
Mike Schlosser <ms122r@gmail.com>
2019-01-24 12:49:46 -0500

    update node script

--- getFormJson.js
+++ getFormJson.js
@@ -4,13 +4,6 @@ const fs = require('fs');
 request('http://dev.polsone.cds-store.com/api/v1/forms', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   console.log(body);
-
-  // Lead Forms - Single Step
-  let data = JSON.stringify(body['leads']);
-  fs.writeFileSync('data/forms/polsone/leads.json', data);
-
-  // Step Forms - Multi Step
-  let stepforms = JSON.stringify(body['step-forms']);
-  fs.writeFileSync('data/forms/polsone/stepforms.json', stepforms);
-
+  let data = JSON.stringify(body);
+  fs.writeFileSync('data/forms/polsone/forms.json', data);
 });
