6ea61bb09e28572f08efb3b4369ee1ae7fa5d548
Mike Schlosser <ms122r@gmail.com>
2019-01-24 12:34:15 -0500

    split forms

--- getFormJson.js
+++ getFormJson.js
@@ -4,6 +4,13 @@ const fs = require('fs');
 request('http://dev.polsone.cds-store.com/api/v1/forms', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   console.log(body);
+
+  // Lead Forms - Single Step
   let data = JSON.stringify(body['leads']);
-  fs.writeFileSync('data/forms/polsone/forms.json', data);
+  fs.writeFileSync('data/forms/polsone/leads.json', data);
+
+  // Step Forms - Multi Step
+  let stepforms = JSON.stringify(body['step-forms']);
+  fs.writeFileSync('data/forms/polsone/stepforms.json', stepforms);
+
 });
