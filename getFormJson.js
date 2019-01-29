04e0e858ed4d91f53dd3b3f626e10418cff86a48
Mike Schlosser <ms122r@gmail.com>
2019-01-29 15:05:22 -0500

    add program by interest area

--- getFormJson.js
+++ getFormJson.js
@@ -29,6 +29,49 @@ request('https://asuonline.asu.edu//lead-submissions-v3.3/programs', { json: tru
   });
   let unique = [...new Set(data)];
   unique = JSON.stringify(unique);
-  console.log(unique);
+  //console.log(unique);
   fs.writeFileSync('data/programs/interestareas.json', unique);
+});
+
+// ASU Programs by Interest Area TEST
+request('https://asuonline.asu.edu//lead-submissions-v3.3/programs', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+
+  let data = {};
+
+  body.forEach(function(element){
+
+  	let interestareas = element.interestareas;
+  	let programtitle = element.title;
+  	let programcode = element.code;
+  	let programcategory = element.category;
+  	let programprogcode = element.progcode;
+  	let programplancode = element.plancode;
+  	let programsubplancode = element.subplancode;
+  	let programconcentration = element.concentration;
+  	let programcrmdestination = element.crmdestination;
+  	let programshortdesc = element.shortdesc;
+  	let programdegreeimage = element.degreeimage;
+  	let programheroimage = element.heroimage;
+  	let programdetailpage = element.detailpage;
+
+  	interestareas.forEach(function(area) {
+  		if(!(area in data)) {
+  			data[area] = []
+  		}
+
+  		let tempProgramObject = {};
+  		tempProgramObject['title'] = programtitle;
+  		tempProgramObject['code'] = programcode;
+  		tempProgramObject['category'] = programcategory;
+  		tempProgramObject['progcode'] = programprogcode;
+  		tempProgramObject['shortdesc'] = programshortdesc;
+
+  		data[area].push(tempProgramObject);
+  	});
+  });
+
+  data = JSON.stringify(data);
+  console.log(data);
+  fs.writeFileSync('data/programs/programsbyinterestareas.json', data);
 });
---
+++ public/dist/js/polsoneVariables.js
@@ -0,0 +1,21 @@
+var frontendValues = {
+	"name__first": "",
+	"name__last": "test",
+	"contact__email": "polsonetest@pearson.com",
+	"frontend__option": [
+    	{
+        	"key": null,
+        	"text": "----------"
+    	},
+    	{
+        	"key": "USC_EMHA",
+        	"text": "Executive Master of Health Administration",
+        	"data_attr": [
+            	{
+               		"key": "program__level",
+                	"value": "graduate"
+            	}
+        	]
+    	}
+	]
+};
